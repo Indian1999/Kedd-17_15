15e82a7160d701df5fbd3c73d2ea109d029f6a98
Indian1999 <haszab99@gmail.com>
2025-02-11 18:40:05 +0100

    szöveg megírva

--- 2ora.py
+++ 2ora.py
@@ -28,3 +28,26 @@ print(szó.upper())      # KUTYA
 print("a cica története".title()) # Minden szót nagybetűvel kezd
 
 print(szöveg.lower().count("a"))
+
+# alt gr + Q -> \
+# \n - enter
+thing_1 = input("Type an object:\n")                # tárgy1
+thing_2 = input("Type another object:\n")           # tárgy2
+adjective = input("Give me an adjective:\n")        # tulajdonság
+song_name = input("Give me the name of a song:\n")  # zene
+celebrity = input("Give me a celebrity name:\n")    # híresség
+feeling = input("Give me a feeling:\n")             # érzés
+verb = input("Give me a verb:\n")                   # ige
+place = input("Give me a place:\n")                 # hely
+food = input("Give me a food item:\n")              # kaja
+person = input("Give me a person/name:\n")          # ember
+
+print(f"""
+I just got back from a pizza party with {1}. 
+Can you believe we got to eat {1} pizza in {1}?! 
+Everyone got to choose their own toppings. 
+I made '{1} and {1}' pizza, which is my favorite! 
+They even stuffed the crust with {1}. How {1}! 
+If that wasn't good enough already, {1} was there singing {1}. 
+I was so inspired by the music, I had to get up out of my seat and {1}.
+""")
