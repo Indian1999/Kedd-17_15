c9f38912b3120d564518e6460a8444398c7b2d71
Indian1999 <haszab99@gmail.com>
2025-02-25 17:44:53 +0100

    ciklusok

---
+++ 4ora.py
@@ -0,0 +1,15 @@
+# Írjuk ki az első 10 természetes számot
+i = 1
+while i <= 10:
+    print(i, end = " ")
+    i += 1
+print()
+
+lista = []
+bemenet = "default"
+while bemenet != "":
+    bemenet = input("Add meg a lista következő elemét!")
+    if bemenet != "":
+        lista.append(bemenet)
+
+print(lista)
