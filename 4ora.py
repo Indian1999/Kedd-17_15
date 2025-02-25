c88e2b27f67e3b0b3298a856ee02201bef6ee748
Indian1999 <haszab99@gmail.com>
2025-02-25 17:57:47 +0100

    listán iterálás

--- 4ora.py
+++ 4ora.py
@@ -22,3 +22,23 @@ print()
 for i in range(5, 15): # i = 5, 6, 7, ... 12, 13, 14 (a 15 már nem)
     print(i, end= " ")
 print()
+
+for i in range(2, 100, 10): # i = 2, 12, 22, 32, ..., 82, 92
+    print(i, end= " ")
+print()
+# range(5) = [0,1,2,3,4]
+# range(5, 10) = [5,6,7,8,9]
+# range(0, 10, 2) = [0,2,4,6,8]
+
+for i in range(100, 10, -10): # i = 100, 90, 80, ..., 30, 20
+    print(i, end= " ")
+print()
+
+lista = ["Anna", "Beáta", "Cecil", "Dénes", "Elemér", "Ferenc"]
+for item in lista: # item = "Anna", "Beáta", ..., "Elemér", "Ferenc"
+    print(item, end = " ")
+print()
+
+for i in range(len(lista)):
+    print(lista[i], end = " ")
+print()
