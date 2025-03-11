793e5e069d6ab2c26c61d37994834b9a40a924bc
Indian1999 <haszab99@gmail.com>
2025-03-11 19:08:42 +0100

    óra vége

--- 6ora(listak).py
+++ 6ora(listak).py
@@ -169,7 +169,9 @@ for i in range(1, 11):
         lista.append(i)
 
 # 8. feladat: [1, -1, 2, -2, 3, -3, ... 15, -15, 16, -16]
-
+for i in range(1, 17):
+    lista.append(i)
+    lista.append(-i)
 
 # 9. feladat: [az első 20 prímszámot tartalmazza]
 
