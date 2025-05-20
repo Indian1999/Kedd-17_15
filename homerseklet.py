356c12c198606f76dbb73dae85e8f14d9da4d743
Indian1999 <haszab99@gmail.com>
2025-05-20 18:23:00 +0200

    feladatok

---
+++ homerseklet.py
@@ -0,0 +1,20 @@
+legalacsonyabb = [-2, 0, -4, 3, 5, 2, 0, -3, 2, 4, 4, 2, 5, 7, 10]
+legmagasabb    = [8, 10, 4, 6, 9, 10, 10, 5, 8, 15, 12, 6, 8, 15, 18]
+
+# 1. feladat: Számítsuk ki minden napra, az adott napi hőingást (legnagyobb - legkisebb)
+# az eredményt tároljuk el
+
+# 2. feladat: Írjuk ki, hogy hanyadik napon volt a legnagyobb hőingás
+
+# 3. feladat: Átlagosan mennyi volt egy nap a legmagasabb hőmérséklet
+
+# 4. feladat: Hány olyan nap volt ahol fagypont alá csökkent a hőmérséklet?
+
+# 5. feladat: Volt-e olyan nap ahol a napi hőingás nem volt nagyobb, mint 5 fok?
+
+# 6. feladat*: Hány olyan nap volt, amikor melegebb volt mint az előző nap 
+# (a legalacsonyabb és legmagasabb hőmérséklet is nagyobb volt)
+
+# 7. feladat: Számoljunk minden napra átlag hőmérsékletet (legnagyobb/legkisebb számtani közepe)
+
+# 8. feladat*: átlagosan hogyan változott napról napra a hőmérséklet 
