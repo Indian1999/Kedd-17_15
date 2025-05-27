75c49157d232dc95296f5832d1ab79810ad075ec
Indian1999 <haszab99@gmail.com>
2025-05-27 19:00:01 +0200

    grafikus done

--- színözön_grafikus.py
+++ színözön_grafikus.py
@@ -11,7 +11,8 @@ def generate_puzzle():
     return puzzle
 
 def check_tipp():
-    tipp = "1234"
+    global tippek
+    tipp = textbox.get()
     if len(tipp) != 4 or not tipp.isdigit() or len(set(tipp)) != 4:
         tkinter.messagebox.showwarning("Hiba", "4 különböző számjegyet adj meg!")
         return
@@ -24,10 +25,44 @@ def check_tipp():
             jo_szamok += 1
     tippek += 1
     if jo_szamok_jo_helyen == 4:
-        pass
+        result_label.config(text=f"Szép volt! {tippek} lépésből kitaláltad, hogy a megoldás {puzzle} volt.")
+        textbox.config(state="disabled")
+        btn_guess.config(state="disabled")
     else:
-        pass
+        feedback = f"{tipp}   ->    jó: {jo_szamok_jo_helyen},    más hely: {jo_szamok}"
+        history.insert(tkinter.END, feedback)
+        textbox.delete(0, tkinter.END)
     
-
 puzzle = generate_puzzle() # pl.: 6843
 tippek = 0
+
+root = tkinter.Tk()
+root.title("Színözön")
+
+main_frame = tkinter.Frame(root)
+main_frame.pack(pady=20, padx=20)
+
+left_frame = tkinter.Frame(main_frame)
+left_frame.grid(row=0, column=0, padx = (0, 20))
+
+right_frame = tkinter.Frame(main_frame)
+right_frame.grid(row = 0, column=1)
+
+tkinter.Label(left_frame, text="Adj meg 4 különböző számjegyet:").pack(pady = 5, padx=5)
+
+textbox = tkinter.Entry(left_frame)
+textbox.pack(pady=5)
+textbox.focus()
+textbox.bind("<Return>", lambda event: check_tipp())
+
+btn_guess = tkinter.Button(left_frame, text = "Tippelés", command=check_tipp)
+btn_guess.pack(pady=5)
+
+result_label = tkinter.Label(left_frame, text = "", fg="green")
+result_label.pack(pady=5)
+
+history = tkinter.Listbox(right_frame, width = 40, height=20)
+history.pack(pady=10)
+
+
+root.mainloop()
