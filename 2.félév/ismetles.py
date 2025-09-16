ac74f0f3fc19b1968581827d35c3f05bb58a0af7
Indian1999 <haszab99@gmail.com>
2025-09-16 17:45:50 +0200

    initial 2. félév

---
+++ 2.félév/ismetles.py
@@ -0,0 +1,14 @@
+print("Szia!")
+
+név = "asd"#input("Hogy hívnak?\n")
+
+print(f"Szia {név}!")
+
+a = 432
+b = 293
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
+print(f"{a} - {b} = {a - b}")
+
