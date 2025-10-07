6c45be716575ea2bc481e504960ef5483bd8bf2d
Indian1999 <haszab99@gmail.com>
2025-10-07 17:50:06 +0200

    Fix matrix data and update decision logic for presence of number 9

--- 2.félév/matrixok.py
+++ 2.félév/matrixok.py
@@ -9,7 +9,7 @@ mtx = [
     [0, 3, 1, 6, 1, 3, 2],
     [7, 3, 0, 6, 4, 0, 3],
     [1, 2, 0, 3, 7, 1, 2],
-    [9, 8, 1, 9, 4, 0, 3]
+    [3, 8, 1, 2, 4, 0, 3]
 ]
 
 print(mtx)
@@ -26,3 +26,17 @@ for i in range(len(mtx)): # sorok száma
     for j in range(len(mtx[i])):
         összeg += mtx[i][j]
 print(összeg)
+
+# Elndöntés tétele: (Van-e 9-es szám a mátrixban?)
+van_kilenc = False
+for i in range(len(mtx)):
+    for j in range(len(mtx[i])):
+        if mtx[i][j] == 9:
+            van_kilenc = True
+            break # kilép a ciklusból
+
+print(van_kilenc)
+
+# Maximum kiválasztás:
+
+# Megszámlálás tétele: (számoljuk meg, hogy hány páratlan szám van a mátrixban)
