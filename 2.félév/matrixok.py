c306cbcbba5245e689a6dd954da2f33ba8c01aa6
Indian1999 <haszab99@gmail.com>
2025-10-07 18:10:28 +0200

    Refactor matrix operations: add maximum selection, odd count, and list comprehension examples

--- 2.félév/matrixok.py
+++ 2.félév/matrixok.py
@@ -38,5 +38,39 @@ for i in range(len(mtx)):
 print(van_kilenc)
 
 # Maximum kiválasztás:
+maximum = mtx[0][0]
+for i in range(len(mtx)):
+    for j in range(len(mtx[i])):
+        if mtx[i][j] > maximum:
+            maximum = mtx[i][j]
+print("maximum:", maximum)
 
 # Megszámlálás tétele: (számoljuk meg, hogy hány páratlan szám van a mátrixban)
+szamlalo = 0
+for i in range(len(mtx)):
+    for j in range(len(mtx[i])):
+        if mtx[i][j] % 2 == 1:
+            szamlalo += 1
+print("Páratlanok:", szamlalo, "db")
+
+
+# List comprehension
+lista = [i**2 for i in range(1, 11)]
+print(lista) # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+mtx = [[i,i**2, "szomszéd"] for i in range(7)]
+print(mtx)
+
+mtx = [[random.randint(0, 100) for j in range(10)] for i in range(8)]
+for row in mtx:
+    print(row)
+
+mtx3d = [[[k for k in range(4)] for j in range(3)] for i in range(5)]
+print(mtx3d)
+
+összeg = 0
+for i in range(len(mtx3d)):
+    for j in range(len(mtx3d[i])):
+        for k in range(len(mtx3d[i][j])):
+            összeg += mtx3d[i][j][k]
+print(összeg)
