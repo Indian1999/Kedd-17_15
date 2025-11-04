6416ecdf278cb253735f0670a60180f203443f99
Indian1999 <haszab99@gmail.com>
2025-11-04 18:58:01 +0100

    sudoku GUI

--- 2.félév/sudoku.py
+++ 2.félév/sudoku.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 board = [
         [7, 8, 0, 4, 0, 0, 1, 2, 0],
         [6, 0, 0, 0, 7, 5, 0, 0, 9],
@@ -17,15 +18,20 @@ def find_empty(board):
                 return i, j
 
 def valid(board, num, row, col):
-    """
-    Igazt ad vissza, ha a board[row][col] helyére be lehet írni legálisan a numot
-    Egyébként hamis értéket ad vissza
-    (Egy sorban csak egyszer szerepelhet egy szám)
-    (Egy oszlolpban csak egyszer szerepelhet egy szám)
-    (Egy kisebb négyzetben szintén csak egszer egy szám)
-    """
-    pass
-
+    for j in range(9):
+        if board[row][j] == num:
+            return False
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    sq_row = row // 3 * 3      # pl.: 5 -> 3,  7 -> 6, 3 -> 3
+    sq_col = col // 3 * 3
+    for i in range(3):
+        for j in range(3):
+            if board[sq_row + i][sq_col + j] == num:
+                return False
+    return True
+    
 def solve(board):
     empty = find_empty(board)
     if empty == None:
@@ -36,8 +42,7 @@ def solve(board):
             board[row][col] = i
             if solve(board):
                 return True
-            else:
-                return False
+            board[row][col] = 0
     return False
 
 def print_board(board):
@@ -51,7 +56,35 @@ def print_board(board):
         print("■")
     print("■ " * 13)
 
-print_board(board)
-solve(board)
-print("Solved:")
-print_board(board)
+root = tk.Tk()
+root.title("Sudoku Solver")
+
+cells = [[None for j in range(9)] for i in range(9)]
+
+def draw_board(pad:int = 3):
+    for i in range(9):
+        for j in range(9):
+            entry = tk.Entry(root, width=3, justify="center")
+            entry.grid(row=i, column=j, padx=pad, pady=pad)
+
+            if board[i][j] != 0:
+                entry.insert(0, str(board[i][j]))
+                entry.config(state="disabled", disabledforeground="black", disabledbackground="gray")
+
+            cells[i][j] = entry
+
+def update_gui():
+    for i in range(9):
+        for j in range(9):
+            cells[i][j].delete(0, tk.END) # 0.tól az utolsó karakterig töröljük a textbox tartalmát
+            cells[i][j].insert(0, str(board[i][j]))
+
+def solve_and_show():
+    solve(board)
+    update_gui()
+
+draw_board(pad = 5)
+solve_button = tk.Button(root, text="Solve", command=solve_and_show)
+solve_button.grid(row=9, column=0, columnspan=9, pady=10)
+root.mainloop()
+
