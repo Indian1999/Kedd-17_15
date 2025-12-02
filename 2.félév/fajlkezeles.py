6faa1ac6baeab20f2f62fdc342b63e1f461e3f7e
Indian1999 <haszab99@gmail.com>
2025-12-02 18:58:11 +0100

    homerseklet

--- 2.félév/fajlkezeles.py
+++ 2.félév/fajlkezeles.py
@@ -51,3 +51,49 @@ with open(output_path, "w", encoding="utf-8") as f:
             else:
                 f.write(f"{matrix[i][j]}")
         f.write("\n")
+
+
+# Olvassuk be és tároljuk el a homerseklet.txt fájlban található adatokat
+homersekletek = []
+path_homerseklet = os.path.join(os.path.dirname(__file__), "homerseklet.txt")
+with open(path_homerseklet, "r", encoding="utf-8") as f:
+    for line in f:
+        line = line.strip().split(";") # ['33', '31', '-1', '3', '3', '-7', '26']
+        sor = [int(item) for item in line]
+        homersekletek.append(sor)
+
+# Hozzunk letre egy homerseklet_elemzes.txt fájlt aminek a tartalma:
+# Az átlag hőmérséklet: x
+# A leghidegebb hőmérséklet: x
+# A leghidegebb nap az év x. napján volt
+# A heti szintű átlaghőmérséklet: [3, 8, -2, 7, 3, 12, ....] (52 elem)
+
+összeg = 0
+day_counter = 0
+for i in range(len(homersekletek)):
+    for j in range(len(homersekletek[i])):
+        összeg += homersekletek[i][j]
+        day_counter += 1
+atlag = összeg / day_counter
+
+leghidegebb = (0, 0)
+for i in range(len(homersekletek)):
+    for j in range(len(homersekletek[i])):
+        mini, minj = leghidegebb
+        if homersekletek[i][j] < homersekletek[mini][minj]:
+            leghidegebb = (i, j)
+nap = leghidegebb[0]*7 + leghidegebb[1] + 1
+
+heti_atlag = []
+for i in range(len(homersekletek)):
+    összeg = 0
+    for j in range(len(homersekletek[i])):
+        összeg += homersekletek[i][j]
+    heti_atlag.append(round(összeg/len(homersekletek[i]),1))
+
+path_homerseklet_elemzes = os.path.join(os.path.dirname(__file__), "homerseklet_elemzes.txt")
+with open(path_homerseklet_elemzes, "w", encoding="utf-8") as f:
+    f.write(f"Az éves átlag hőmérséklet: {round(atlag, 1)}\n")
+    f.write(f"Az év leghidegebb napján {homersekletek[leghidegebb[0]][leghidegebb[1]]} °C volt.\n")
+    f.write(f"Ez a nap az év {nap}. napja volt.\n")
+    f.write(f"A heti szintű átlag hőmérsékletek: {heti_atlag}\n")
