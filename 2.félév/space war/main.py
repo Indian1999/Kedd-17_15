3917d91a59ef06c4f8ea257d1dce283aca2dc5e7
Indian1999 <haszab99@gmail.com>
2026-01-06 18:56:34 +0100

    movement

--- 2.félév/space war/main.py
+++ 2.félév/space war/main.py
@@ -6,10 +6,18 @@ import math
 WIDTH = 900
 HEIGHT = 500
 
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE  = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
 SPACESHIP_WIDTH = WIDTH // 11
 SPACESHIP_HEIGHT = HEIGHT // 7
 
 FPS = 60
+VELOCITY = 5
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space War")
@@ -27,13 +35,38 @@ YELLOW_SPACESHIP = pygame.image.load(os.path.join(ASSETS, "spaceship_yellow.png"
 YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
 YELLOW_SPACESHIP = pygame.transform.rotate(YELLOW_SPACESHIP, 90)
 
+BORDER = pygame.Rect(WIDTH // 2 - 7, 0, 14, HEIGHT)
+
 def draw_frame(red, yellow):
     WINDOW.blit(BACKGROUND, (0,0))
+
+    pygame.draw.rect(WINDOW, BLACK, BORDER)
 
     WINDOW.blit(RED_SPACESHIP, (red.x, red.y))
     WINDOW.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
 
     pygame.display.update()
+
+    
+def red_controll(keys_pressed, red):
+    if keys_pressed[pygame.K_a] and red.x > 5:
+        red.x -= VELOCITY
+    if keys_pressed[pygame.K_d] and red.x < WIDTH // 2 - SPACESHIP_WIDTH:
+        red.x += VELOCITY
+    if keys_pressed[pygame.K_w] and red.y > 5:
+        red.y -= VELOCITY
+    if keys_pressed[pygame.K_s] and red.y < HEIGHT - SPACESHIP_HEIGHT - 5:
+        red.y += VELOCITY
+
+def yellow_controll(keys_pressed, yellow):
+    if keys_pressed[pygame.K_LEFT] and yellow.x > WIDTH // 2 + 5:
+        yellow.x -= VELOCITY
+    if keys_pressed[pygame.K_RIGHT] and yellow.x < WIDTH - SPACESHIP_WIDTH - 5:
+        yellow.x += VELOCITY
+    if keys_pressed[pygame.K_UP] and yellow.y > 5:
+        yellow.y -= VELOCITY
+    if keys_pressed[pygame.K_DOWN] and yellow.y < HEIGHT - SPACESHIP_HEIGHT - 5:
+        yellow.y += VELOCITY
 
 def main():
     red = pygame.Rect(20, HEIGHT // 2 - SPACESHIP_HEIGHT // 2, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -47,7 +80,13 @@ def main():
                 gameOn = False
                 pygame.quit()
                 exit()
+            if event.type == pygame.KEYDOWN:
+                pass
         
+        keys_pressed = pygame.key.get_pressed()
+        red_controll(keys_pressed, red)
+        yellow_controll(keys_pressed, yellow)
+
         draw_frame(red, yellow)
 
 if __name__ == "__main__":
