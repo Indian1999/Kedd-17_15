39d846efe3020908789dc57a4101904c0416fd8c
Indian1999 <haszab99@gmail.com>
2026-03-17 18:59:49 +0100

    snake base done

--- 3.félév/snake.py
+++ 3.félév/snake.py
@@ -1,4 +1,27 @@
 import pygame
+import random
+pygame.font.init()
+
+class Food:
+    def __init__(self, width, height, pixel_size):
+        self.width = width
+        self.height = height
+        self.pixel_size = pixel_size
+        self.random_pos()
+
+    def random_pos(self, banned_positions = []):
+        self.x = random.randint(0, self.width) // self.pixel_size * self.pixel_size
+        self.y = random.randint(0, self.height) // self.pixel_size * self.pixel_size
+        while (self.x, self.y) in banned_positions:
+            self.x = random.randint(0, self.width) // self.pixel_size * self.pixel_size
+            self.y = random.randint(0, self.height) // self.pixel_size * self.pixel_size
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self, window, pixel_size, color = (20, 190, 20)):
+        pygame.draw.rect(window, color, pygame.Rect(self.x, self.y, pixel_size, pixel_size))
 
 class Snake:
     def __init__(self, x, y):
@@ -7,13 +30,34 @@ class Snake:
         self.length = 1
         self.x_vel = 0
         self.y_vel = 0
-        self.body = []
+        self.body = [(self.x, self.y)] # (10, 15)
+        self.length = 1
 
     def move(self):
         self.x += self.x_vel
         self.y += self.y_vel
+        self.body.append((self.x, self.y))
 
+    def remove_tail(self):
+        if len(self.body) > self.length:
+            self.body.pop(0)
 
+    def increase_length(self):
+        self.length += 1
+
+    def is_touching(self, food: Food):
+        return food.x == self.x and food.y == self.y
+    
+    def is_self_bitten(self):
+        return (self.x, self.y) in self.body[:-1]
+
+    def draw(self, window, pixel_size, color = (0,0,0)):
+        for pixel in self.body:
+            pygame.draw.rect(window, color, pygame.Rect(pixel[0], pixel[1], pixel_size, pixel_size))
+
+    def __len__(self):
+        return self.length
+        
 
 class SnakeGame:
     WHITE = (255,255,255)
@@ -37,40 +81,88 @@ class SnakeGame:
     def draw_frame(self):
         self.window.fill(SnakeGame.BLUE)
 
+        self.snake.draw(self.window, self.pixel_size, SnakeGame.BLACK)
+        self.food.draw(self.window, self.pixel_size, SnakeGame.GREEN)
+
+        pygame.display.update()
+
+    def draw_game_over_screen(self):
+        self.window.fill(SnakeGame.BLUE)
+
+        game_over_font = pygame.font.SysFont("Arial", self.pixel_size)
+        score_font = pygame.font.SysFont("Arial", round(self.pixel_size * 1.25))
+
+        game_over_text = game_over_font.render("R: Újraindítás | Q: Kilépés", True, SnakeGame.RED)
+        score_text = score_font.render(f"Score: {self.snake.length - 1}", True, SnakeGame.WHITE)
+
+        self.window.blit(game_over_text,
+                         [self.WIDTH//2 - game_over_text.get_width() // 2,
+                          self.HEIGHT//2 - game_over_text.get_height()])
+        self.window.blit(score_text,
+                         [self.WIDTH//2 - score_text.get_width() // 2,
+                          self.HEIGHT//2 + score_text.get_height()])
+
         pygame.display.update()
 
     def game_loop(self):
         game_over = False
         application_close = False
 
-        snake = Snake(
+        self.snake = Snake(
             x = round(self.WIDTH // 2 / self.pixel_size) * self.pixel_size,
             y= round(self.HEIGHT // 2 / self.pixel_size) * self.pixel_size
         )
 
+        self.food = Food(self.WIDTH, self.HEIGHT, self.pixel_size)
+
         while not application_close:
             SnakeGame.clock.tick(self.speed)
-            
+
+            while game_over:
+                self.draw_game_over_screen()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        application_close = True
+                        return
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_q:
+                            pygame.quit()
+                            quit()
+                        if event.key == pygame.K_r:
+                            self.game_loop()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     application_close = True
                     return
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        snake.x_vel = -1
-                        snake.y_vel = 0
-                    if event.key == pygame.K_RIGHT:
-                        snake.x_vel = 1
-                        snake.y_vel = 0
-                    if event.key == pygame.K_UP:
-                        snake.x_vel = 0
-                        snake.y_vel = -1
-                    if event.key == pygame.K_DOWN:
-                        snake.x_vel = 0
-                        snake.y_vel = 1
+                    if event.key == pygame.K_LEFT and self.snake.x_vel != self.pixel_size:
+                        self.snake.x_vel = -self.pixel_size
+                        self.snake.y_vel = 0
+                    if event.key == pygame.K_RIGHT and self.snake.x_vel != -self.pixel_size:
+                        self.snake.x_vel = self.pixel_size
+                        self.snake.y_vel = 0
+                    if event.key == pygame.K_UP and self.snake.y_vel != self.pixel_size:
+                        self.snake.x_vel = 0
+                        self.snake.y_vel = -self.pixel_size
+                    if event.key == pygame.K_DOWN and self.snake.y_vel != -self.pixel_size:
+                        self.snake.x_vel = 0
+                        self.snake.y_vel = self.pixel_size
             
-            snake.move()
+            self.snake.move()
+
+            if self.snake.x < 0 or self.snake.x > self.WIDTH or self.snake.y < 0 or self.snake.y > self.HEIGHT:
+                game_over = True
+
+            if self.snake.is_touching(self.food):
+                self.snake.increase_length()
+                self.food.random_pos()
                 
+            self.snake.remove_tail()
+
+            if self.snake.is_self_bitten():
+                game_over = True
+            
             self.draw_frame()
 
 
