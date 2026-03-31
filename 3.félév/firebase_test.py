0eaad098b136f16423be33220099b5b10f49c30b
Indian1999 <haszab99@gmail.com>
2026-03-31 18:05:23 +0200

    firebase bevezető

---
+++ 3.félév/firebase_test.py
@@ -0,0 +1,29 @@
+import requests
+import time
+
+url = "https://snake-kedd-default-rtdb.europe-west1.firebasedatabase.app/highscores.json"
+
+def get_highscores():
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("ERROR:", response.status_code)
+
+def post_highscore(name, score):
+    data = {
+        "name":name,
+        "score":score,
+        "timestamp": int(time.time())
+    }
+
+    response = requests.post(url, json=data)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("ERROR:", response.status_code)
+
+print(post_highscore("Dani", 8))
+print(get_highscores())
